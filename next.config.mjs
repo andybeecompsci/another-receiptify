6070b2e802ce78bb1d773613e3675931701a1261
Anderson Bee <n10791051@qut.edu.au>
2024-12-03 10:36:43 +1000

    deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
-  },
-  experimental: {
-    swcPlugins: [],
-  },
+  }
 }
 
 export default nextConfig;

ed1cad224bc6517ad47521648c0d47f69f44b4bb
Anderson Bee <n10791051@qut.edu.au>
2024-12-03 10:34:52 +1000

    deploymentnerror fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  experimental: {
+    swcPlugins: [],
+  },
+}
 
 export default nextConfig;
